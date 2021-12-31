e301fce41e6119db2b5be2bf1163705565c9752e
Keima <k.ishibashi@4nonome.com>
2021-12-31 18:27:26 +0900

    .env setting

--- twitter/settings.py
+++ twitter/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+#.envファイルを読み込み
+env = environ.Env()
+environ.Env.read_env(Path.joinpath(BASE_DIR, '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)!w^hk6c90&z@kp-xol62gd&o(_ey8l*$8pmckzz!=h)z+j4rp'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
