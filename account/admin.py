28c49be35de2fd9186e7c9b90d04895381518c3e
I-Keima <k1e4i8m4a@gmail.com>
2022-01-01 14:59:56 +0900

    fix

--- account/admin.py
+++ account/admin.py
@@ -9,27 +9,28 @@ from account.models import MyUser
 
 
 class UserCreationForm(forms.ModelForm):
-	password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
-	password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput)
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput)
 
-	class Meta:
-		model = MyUser
-		fields = ("email", "date_of_birth")
+    class Meta:
+        model = MyUser
+        fields = ("email", "date_of_birth")
 
-	def clean_password2(self):
-		password1 = self.cleaned_data.get("password1")
-		password2 = self.cleaned_data.get("password2")
-		if password1 and password2 and password1 != password2:
-			raise ValidationError("パスワードが一致しません")
-		return password2
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("パスワードが一致しません")
+        return password2
 		
-	def save(self, commit=True):
-		user = super().save(commit=False)
-		user.get_password(self.cleaned_data["password1"])
-		if commit:
-			user.save()
-		return user
-	
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        # getじゃなくてset
+        if commit:
+            user.save()
+        return user
+
 # 以下面倒になったのでコピペ
 
 class UserChangeForm(forms.ModelForm):
--- twitter/settings.py
+++ twitter/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'twitter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "account.MyUser"
+
+# LOGIN_REDIRECT_URL =
